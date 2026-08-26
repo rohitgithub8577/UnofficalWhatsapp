9040f32a4db4ebbfc557141d687efffa045cf98a
Rohit Jaiswal <167600220+rohitgithub8577@users.noreply.github.com>
2026-08-26 19:42:40 +0530

    Add files via upload

--- dbconfig.js
+++ dbconfig.js
@@ -1,28 +1,8 @@
-// THis Connect is MsSql
-// const config = {
-//     server: "DESKTOP-SDENQMO\\MSSQLSERVER01", 
-//     database: "waba",
-//     user: "sa",                                 
-//     password: "master",                         
-//     options: {                                  
-//         trustServerCertificate: true,
-//         encrypt: false                          
-//     },
-//     port: 1433                                   
-// };
-
-// module.exports = config;
-
-
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "dbwhatsapp",
-    port: 3306
-});
+const connection = mysql.createConnection(
+    process.env.MYSQL_URL
+);
 
 connection.connect((err) => {
     if (err) {
@@ -36,3 +16,29 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
+
+
+
+
+// const mysql = require("mysql2");
+
+// const connection = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "admin",
+//     database: "dbwhatsapp",
+//     port: 3306
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.error("❌ MySQL Connection Failed:");
+//         console.error("Error Code:", err.code);
+//         console.error("Error Message:", err.message);
+//         return;
+//     }
+
+//     console.log("✅ MySQL Connected Successfully");
+// });
+
+// module.exports = connection;
